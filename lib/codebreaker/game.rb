8bc3854cad63c820e68efe55feb503267ec20a7d
Mikkko <danilzhmak88@gmail.com>
2020-07-08 18:09:40 +0300

    added method to create secret code, added guess validation

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,31 +1,56 @@
-require 'pry'
-
 module Codebreaker
   class Game
-    attr_accessor :secret, :hints, :max_attempts, :answer, :attempts, :status
+    include Validator
+    attr_accessor :secret, :hints, :answer, :attempts, :status, :player
 
-    def initialize
-      @secret = []
-      @hints = 0
-      @attempts = 0
-      @answer = ''
+    def initialize(_player, difficulty)
+      @secret = create_secret
+      difficulty_changer(difficulty)
+      @status = :game
     end
 
-    def difficulty(difficulty_name)
-      case difficulty_name.to_s.capitalize
-      when 'Easy' then change_difficulty(2, 15)
-      when 'Medium' then change_difficulty(1, 10)
-      when 'Hell' then change_difficulty(1, 5)
-      else
-        raise ArgumentError, 'Difficulty must be Easy, Medium or Hell'
-      end
+    def guess(answer)
+      check_status
+      validate_guess(answer)
+      @attempts -= 1
     end
 
     private
 
-    def change_difficulty(hints, max_attempts)
+    def difficulty_changer(difficulty_name)
+      validate_difficulty(difficulty_name)
+      case difficulty_name.to_s.capitalize
+      when 'Easy' then change_difficulty(2, 15)
+      when 'Medium' then change_difficulty(1, 10)
+      when 'Hell' then change_difficulty(1, 5)
+      end
+    end
+
+    def validate_guess(answer)
+      validate_argument_type(answer, Integer)
+    end
+
+    def create_secret
+      secret = []
+      4.times { secret << rand(1..6) }
+      secret
+    end
+
+    def change_difficulty(hints, attempts)
       @hints = hints
-      @max_attempts = max_attempts
+      @attempts = attempts
+    end
+
+    def check_status
+      if @attempts.zero?
+        @status = :lose
+      elsif @result == '++++'
+        @status = :win
+      end
+    end
+
+    def break_number(number)
+      number.to_s.scan(/./).map(&:to_i)
     end
   end
 end

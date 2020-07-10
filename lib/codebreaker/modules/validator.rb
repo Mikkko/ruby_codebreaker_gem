81f00356047fd3775ed0859cfffc19a668fbd98c
Mikkko <danilzhmak88@gmail.com>
2020-07-10 13:03:05 +0300

    edited

--- lib/codebreaker/modules/validator.rb
+++ lib/codebreaker/modules/validator.rb
@@ -1,13 +1,21 @@
 module Codebreaker
   module Validator
     include Errors
-    def validate_argument_type(answer, class_name)
-      raise WrongArgumentError until answer.is_a?(class_name)
+    def validate_argument_type(argument, class_name)
+      raise WrongArgumentError until argument.is_a?(class_name)
     end
 
     def validate_difficulty(difficulty_given)
       difficulty = %w[Easy Medium Hell]
       raise DifficultyError until difficulty.include?(difficulty_given)
+    end
+
+    def validate_length(argument, length_argument)
+      raise LengthError until argument.digits.count == length_argument
+    end
+
+    def validate_range(number, range)
+      raise RangeError until number.digits.each { |digit| range.include?(digit) }
     end
   end
 end

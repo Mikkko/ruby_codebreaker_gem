8a9a340e938b902af0177b211c4b960e48627438
Mikkko <danilzhmak88@gmail.com>
2020-07-18 17:50:15 +0300

    added name validation

--- lib/codebreaker/player.rb
+++ lib/codebreaker/player.rb
@@ -1,9 +1,20 @@
 module Codebreaker
   class Player
+    include Validator
     attr_accessor :name
 
+    NAME_LENGTH = (3..20).freeze
+
     def initialize(name)
+      validate_player(name)
       @name = name
+    end
+
+    private
+
+    def validate_player(name)
+      validate_argument_type(name, String)
+      validate_string_length(name, NAME_LENGTH)
     end
   end
 end

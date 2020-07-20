f0e3e8a7ca4959d6428b7efefc52561afd933147
Mikkko <danilzhmak88@gmail.com>
2020-07-20 19:46:41 +0300

    removed to classes

---
+++ lib/codebreaker/classes/statistic.rb
@@ -0,0 +1,19 @@
+module Codebreaker
+  class Statistic
+    def initialize(file_path)
+      @file_path = file_path
+    end
+
+    def store_statistic(hash)
+      stats = File.file?(@file_path) && !File.zero?(@file_path) ? load_statistic : []
+      stats << hash
+      file = File.open(@file_path, 'w')
+      file.write(stats.to_yaml)
+      file.close
+    end
+
+    def load_statistic
+      File.file?(@file_path) && !File.zero?(@file_path) ? YAML.load_file(@file_path) : []
+    end
+  end
+end

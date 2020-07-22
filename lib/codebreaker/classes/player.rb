3d5eca4c91c449f438dac8d798faada4a8fbcf04
Mikkko <danilzhmak88@gmail.com>
2020-07-23 01:05:05 +0300

    renamed validate_player method to validate

--- lib/codebreaker/classes/player.rb
+++ lib/codebreaker/classes/player.rb
@@ -6,13 +6,13 @@ module Codebreaker
     NAME_LENGTH = (3..20).freeze
 
     def initialize(name)
-      validate_player(name)
+      validate(name)
       @name = name
     end
 
     private
 
-    def validate_player(name)
+    def validate(name)
       validate_argument_type(name, String)
       validate_string_length(name, NAME_LENGTH)
     end

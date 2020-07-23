088b3d740ea549e4e655dcb75fac16289351d916
Mikkko <danilzhmak88@gmail.com>
2020-07-23 13:42:58 +0300

    edited player spec

--- spec/codebreaker/player_spec.rb
+++ spec/codebreaker/player_spec.rb
@@ -6,8 +6,18 @@ RSpec.describe Codebreaker::Player do
       expect(player.instance_variables).to include(:@name)
     end
 
-    it 'name is Mikkko' do
-      expect(player.name).to eq('Mikkko')
+    it 'name is string' do
+      expect(player.name.class).to eq(String)
+    end
+  end
+
+  describe '.validate' do
+    it 'raises WrongArgumentError' do
+      expect { described_class.new(123) }.to raise_error(Codebreaker::Errors::WrongArgumentError)
+    end
+
+    it 'raises ClassError' do
+      expect { described_class.new('ar') }.to raise_error(Codebreaker::Errors::LengthError)
     end
   end
 end

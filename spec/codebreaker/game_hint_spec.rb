15478d06b25eb916fa47f20e5ea7d8b991ac2377
Mikkko <danilzhmak88@gmail.com>
2020-07-24 14:54:20 +0300

    updated hint spec

--- spec/codebreaker/game_hint_spec.rb
+++ spec/codebreaker/game_hint_spec.rb
@@ -2,16 +2,25 @@ RSpec.describe Codebreaker::Game do
   let(:game) { described_class.new(Codebreaker::Player.new('Mikkko'), 'easy') }
 
   describe '#hint' do
-    it 'returns integer' do
-      expect(game.hint.class).to eq(Integer)
-    end
-
-    it 'returns one random number from secret code' do
-      expect(game.secret).to include(game.hint)
+    it 'raise HintError if hints are absent' do
+      game.instance_variable_set(:@hints, 0)
+      expect { game.hint }.to raise_error(Codebreaker::Errors::HintError)
     end
 
     it 'reduces hint counter by 1' do
       expect { game.hint }.to change(game, :hints).by(-1)
+    end
+
+    it 'returns integer' do
+      expect(game.hint.class).to eq(Integer)
+    end
+
+    it 'reduces hint_number size by 1' do
+      expect { game.hint }.to change { game.hint_number.size }.by(-1)
+    end
+
+    it 'returns number included in secret_code' do
+      expect(game.secret_code).to include(game.hint)
     end
   end
 end

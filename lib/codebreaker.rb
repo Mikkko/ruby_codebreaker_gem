be15dcf06e5e0bbcb319672298f36d1deb59bc9f
Mikkko <danilzhmak88@gmail.com>
2020-07-27 17:10:56 +0300

    added spaces to make file more readable

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,12 +1,15 @@
 require 'codebreaker/version'
 require 'yaml/store'
+
 require_relative 'codebreaker/errors/statistic_file_error'
 require_relative 'codebreaker/errors/difficulty_error'
 require_relative 'codebreaker/errors/wrong_argument_error'
 require_relative 'codebreaker/errors/length_error'
 require_relative 'codebreaker/errors/range_error'
 require_relative 'codebreaker/errors/hint_error'
+
 require_relative 'codebreaker/modules/validator'
+
 require_relative 'codebreaker/classes/difficulty'
 require_relative 'codebreaker/classes/player'
 require_relative 'codebreaker/classes/guess'

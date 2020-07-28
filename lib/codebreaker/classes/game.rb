54322d65932cd0fb135d2e69ec2a2225910b9ec1
Mikkko <danilzhmak88@gmail.com>
2020-07-28 14:26:40 +0300

    removed status

--- lib/codebreaker/classes/game.rb
+++ lib/codebreaker/classes/game.rb
@@ -4,11 +4,7 @@ module Codebreaker
     include Errors
     SECRET_CODE_SIZE = 4
     SECRET_CODE_RANGE = (1..6).freeze
-    WIN_COMBINATION = '++++'.freeze
-    WIN_GAME_STATUS = :win
-    LOSE_GAME_STATUS = :lose
-    IN_GAME_STATUS = :game
-    attr_reader :secret_code, :hints, :hint_number, :attempts, :max_attempts, :status, :player, :result, :difficulty
+    attr_reader :secret_code, :hints, :hint_number, :attempts, :max_attempts, :player, :result, :difficulty
 
     def initialize(player, difficulty)
       @difficulty = Difficulty.new(difficulty)
@@ -16,14 +12,12 @@ module Codebreaker
       @hints = @difficulty.hints
       @secret_code = generate_secret_code
       @hint_number = @secret_code.clone
-      @status = IN_GAME_STATUS
       @player = player
     end
 
     def guess(answer)
       @attempts -= 1
       @result = Guess.new(answer, @secret_code).check_guess
-      @status = check_game_status
       @result
     end
 
@@ -41,13 +35,6 @@ module Codebreaker
 
     def generate_secret_code
       Array.new(SECRET_CODE_SIZE) { rand(SECRET_CODE_RANGE) }
-    end
-
-    def check_game_status
-      if @attempts.zero? then LOSE_GAME_STATUS
-      elsif @result == WIN_COMBINATION then WIN_GAME_STATUS
-      else IN_GAME_STATUS
-      end
     end
   end
 end

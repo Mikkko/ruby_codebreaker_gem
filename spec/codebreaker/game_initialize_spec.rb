f1a4f6932c73c782d0b723c209e26995cbdfc330
Mikkko <danilzhmak88@gmail.com>
2020-07-28 14:26:52 +0300

    removed status check

--- spec/codebreaker/game_initialize_spec.rb
+++ spec/codebreaker/game_initialize_spec.rb
@@ -22,10 +22,6 @@ RSpec.describe Codebreaker::Game do
       expect(game.difficulty.class).to eq(Codebreaker::Difficulty)
     end
 
-    it 'game status equal to IN_GAME_STATUS' do
-      expect(game.status).to eq(described_class::IN_GAME_STATUS)
-    end
-
     it 'hint_number is equal to secret_code' do
       expect(game.hint_number).to eq(game.secret_code)
     end
